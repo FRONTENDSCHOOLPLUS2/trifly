54ec17077227f9ad0045f0725c4cef47046be11c
JWJung-99 <39busy@naver.com>
2024-08-08 17:12:56 +0900

    ✅ done: Header 컴포넌트 작업 완료

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,14 +1,48 @@
 import Link from "next/link";
 import "./Header.scss";
 
-const Header = ({ type = "white" }) => {
+interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  type?: "default" | "primary" | "transparent";
+}
+
+const Header: React.FC<HeaderProps> = ({ type = "default" }) => {
   return (
     <header className={`header ${type}Type`}>
-      <div className="layout">
-        Header
-        <Link href={`/`}>메인</Link>
-        <Link href={`/order`}>결제</Link>
-        <Link href={`/footprint`}>발자국</Link>
+      <div className="layout header-contents">
+        <div className="header-logo">
+          <Link href={`/`}>
+            <img src={`logo-${type}.svg`} alt="TriFly" />
+            <i className="hidden">메인</i>
+          </Link>
+        </div>
+
+        <nav className="header-nav">
+          <Link href={`/order`}>예약내역</Link>
+          <Link href={`/footprint`}>발자국</Link>
+        </nav>
+
+        <div className="header-user">
+          {/* {user ? (
+            <button type="button">
+              <img src={`icon-logout-${type === "default" ? "black" : "white"}.svg`} alt="로그아웃" />
+              <i className="hidden">로그아웃</i>
+            </button>
+          ) : (
+            <Link href={`/login`}>
+              <img src={`icon-login-${type === "default" ? "black" : "white"}.svg`} alt="로그인" />
+              <i className="hidden">로그인</i>
+            </Link>
+          )} */}
+
+          <Link href={`/login`}>
+            <img
+              src={`icon-login-${type === "default" ? "black" : "white"}.svg`}
+              alt="로그인"
+            />
+            <i className="hidden">로그인</i>
+          </Link>
+        </div>
+
         <div className="corner">
           <div className="left"></div>
           <div className="right"></div>
