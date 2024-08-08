c50051025e31f6ed982a987c6d8a25a8fc59e096
JWJung-99 <39busy@naver.com>
2024-08-08 13:43:32 +0900

    ✅ done: 버튼 컴포넌트 작업 완료

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,14 @@
 import "./Button.scss";
 
-const Button = ({
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  type?: "button" | "submit";
+  size?: "xs" | "sm" | "md" | "lg" | "full";
+  bgColor?: "primary" | "secondary" | "gray" | "light";
+}
+
+const Button: React.FC<ButtonProps> = ({
   children,
   type = "button",
   size = "sm",
