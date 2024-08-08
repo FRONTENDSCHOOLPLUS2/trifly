1b20a27cf7f53b638f8701ef893927648d0c7806
JWJung-99 <39busy@naver.com>
2024-08-08 18:07:43 +0900

    ✅ done: 버튼형 Link 컴포넌트 작업 완료

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -1,7 +1,19 @@
 import Link from "next/link";
 import "./Anchor.scss";
 
-const Anchor = ({ children, size = "full", bgColor = "primary", href }) => {
+interface AnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: React.ReactNode;
+  href: string;
+  size?: "xs" | "sm" | "md" | "lg" | "full";
+  bgColor?: "primary" | "gray";
+}
+
+const Anchor: React.FC<AnchorProps> = ({
+  children,
+  size = "full",
+  bgColor = "primary",
+  href,
+}) => {
   return (
     <Link className={`anchor ${size} ${bgColor}`} href={href}>
       {children}
