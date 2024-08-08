ed0a58ac94f0f0abccbe5f1b3f944fbccd4185f2
s0zzang <dlth8888@gmail.com>
2024-08-08 13:50:16 +0900

    💄 style: primary, transparent 레이아웃 세팅

---
+++ src/app/(primary)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header/Header";
+
+export default function PrimaryLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header type="primary" />
+      <main className="containerLayout layout">{children}</main>
+    </>
+  );
+}
