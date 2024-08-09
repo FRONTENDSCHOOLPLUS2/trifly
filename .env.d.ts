57108dce68c14b1fe59de3e0cc63dbd42aafdfcf
s0zzang <dlth8888@gmail.com>
2024-08-09 13:56:43 +0900

    🔒 type: env 타입 정의

---
+++ .env.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    MARKET_API_SERVER = string;
+    MARKET_API_CLIENT_ID = string;
+    GITHUB_CLIENT_ID = string;
+    GITHUB_CLIENT_SECRET = string;
+    GOOGLE_CLIENT_ID = string;
+    GOOGLE_CLIENT_SECRET = string;
+  }
+}
