ee050b4f45e2c674cdec4d30ed6199d596256250
JWJung-99 <39busy@naver.com>
2024-08-09 19:47:59 +0900

    ✨ feat: 메인화면 생성

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,5 +1,24 @@
+import "./main.scss";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="main">
+      <h2 className="hidden">메인</h2>
+      <section className="search-container">
+        <h3 className="hidden">검색창</h3>
+      </section>
+
+      <section className="banner">
+        <h3 className="hidden">배너-포토티켓 꾸미기</h3>
+        배너
+      </section>
+
+      <section className="notices">
+        <h3 className="hidden">공지사항</h3>
+        공지사항
+      </section>
+    </div>
+  );
 };
 
 export default Home;
