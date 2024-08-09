7e77f23b1d8c137064893c00289bd44c45f173b8
s0zzang <dlth8888@gmail.com>
2024-08-09 20:16:18 +0900

    ✅ done: progress 컴포넌트

---
+++ src/components/Progress/Progress.tsx
@@ -0,0 +1,20 @@
+import "./progress.scss";
+
+const Progress = ({
+  textArr,
+  status = 4,
+}: {
+  textArr: string[];
+  status: number;
+}) => {
+  const li = textArr.map((item, idx) => {
+    return <li className={idx + 1 <= status ? "act" : ""}>{item}</li>;
+  });
+  return (
+    <div className="progress">
+      <ul>{li}</ul>
+    </div>
+  );
+};
+
+export default Progress;
