8d424c24045a1ff887cdf749011364294ddef7de
JWJung-99 <39busy@naver.com>
2024-08-09 14:33:55 +0900

    🔧 conf: env.d.ts 파일 수정

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_MARKET_API_SERVER: string;
+    MARKET_API_CLIENT_ID: string;
+    KAKAO_CLIENT_ID: string;
+    KAKAO_CLIENT_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    NAVER_CLIENT_ID: string;
+    NAVER_CLIENT_SECRET: string;
+  }
+}
