4877e7824fb3fbdcf9b46b456f9aa9edfe1f8b50
JWJung-99 <39busy@naver.com>
2024-08-12 00:56:13 +0900

    ✅ done: 메인화면 공지사항 UI 작업 완료

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -26,19 +26,19 @@ const Home = async () => {
           </Link>
         </div>
 
-        <div>
-          <Link href="/notices/1">
-            <span>HOT</span>
-            <span>
+        <div className="notice-items">
+          <div className="notice-item">
+            <span className="badge primary">HOT</span>
+            <Link href="/notices/1">
               최초 발권 후 24시 이내 환불 처리 방침 변경 항공(국제선) 안내
-            </span>
-            <span className="pc">2024.05.01</span>
-          </Link>
-          <Link href="/notices/2">
-            <span>HOT</span>
-            <span>2024년 8월 유류할증 안내</span>
-            <span className="pc">2024.07.12</span>
-          </Link>
+            </Link>
+            <p className="pc">2024.05.01</p>
+          </div>
+          <div className="notice-item">
+            <span className="badge primary">HOT</span>
+            <Link href="/notices/2">2024년 8월 유류할증 안내</Link>
+            <p className="pc">2024.07.12</p>
+          </div>
         </div>
       </section>
     </div>
