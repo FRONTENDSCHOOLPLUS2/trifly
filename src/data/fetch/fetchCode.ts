3530d6c08321a0b4e90663d398cc3e8234cb7d1c
JWJung-99 <39busy@naver.com>
2024-08-14 00:53:42 +0900

    🔒 type: fetchCode 함수 수정

--- src/data/fetch/fetchCode.ts
+++ src/data/fetch/fetchCode.ts
@@ -1,9 +1,14 @@
-import { ApiRes, CodeData, CodeState, SingleItem } from "@/types";
+import { AirportData, ApiRes, CodeData, CodeState, SingleItem } from "@/types";
 
 const SERVER = process.env.NEXT_PUBLIC_MARKET_API_SERVER;
 const CLIENT_ID = process.env.NEXT_PUBLIC_MARKET_API_CLIENT_ID;
 
-export const fetchCodes = async (): Promise<CodeState> => {
+interface CodeResult {
+  code: CodeState;
+  airportCode: AirportData[];
+}
+
+export const fetchCodes = async (): Promise<CodeResult> => {
   const url = `${SERVER}/codes`;
   const res = await fetch(url, {
     headers: {
@@ -16,5 +21,8 @@ export const fetchCodes = async (): Promise<CodeState> => {
     throw new Error("코드 조회 실패!");
   }
 
-  return resJson.item.flatten;
+  return {
+    code: resJson.item.flatten,
+    airportCode: resJson.item.nested.airport.codes,
+  };
 };
