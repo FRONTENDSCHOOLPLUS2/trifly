7bac58b68d7fc7bd0764b76cd1402493ded4294d
JWJung-99 <39busy@naver.com>
2024-08-13 11:03:54 +0900

    🚧 cont: 티켓 검색 컴포넌트 작업 중

--- src/components/TicketSearch/TicketSearch.tsx
+++ src/components/TicketSearch/TicketSearch.tsx
@@ -1,7 +1,14 @@
 import "./TicketSearch.scss";
 
 const TicketSearch = () => {
-  return <div className="search-box">티켓 검색</div>;
+  return (
+    <div className="search-layout">
+      <form className="search-wrapper">
+        <div className="search-header">왕복 편도 헤더</div>
+        <div className="search-main">출/도착, 날짜, 인원, 검색</div>
+      </form>
+    </div>
+  );
 };
 
 export default TicketSearch;
