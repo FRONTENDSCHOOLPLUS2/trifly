ff905596f49e60a7926cb27e575947cf6b9002f3
s0zzang <dlth8888@gmail.com>
2024-08-13 14:41:31 +0900

    ✅ done: orderStatus context 생성

---
+++ src/app/(primary)/order/agree/page.tsx
@@ -0,0 +1,7 @@
+const AgreePage = async () => {
+  // const data =
+
+  return <div>AgreePage</div>;
+};
+
+export default AgreePage;
---
+++ src/app/(primary)/order/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Progress from "@/components/Progress/Progress";
+import { useMemo, useState } from "react";
+import "./order.scss";
+import OrderContext from "./orderContext";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  const [orderStatus, setOrderStatus] = useState(1);
+  const orderPages = [
+    "약관 동의",
+    "정보 입력 및 결제",
+    "좌석 선택",
+    "예약 완료",
+  ];
+
+  const contextValue = useMemo(
+    () => ({
+      orderStatus,
+      setOrderStatus,
+    }),
+    [orderStatus]
+  );
+
+  return (
+    <div className="order">
+      <Progress textArr={orderPages} status={orderStatus} />
+      <OrderContext.Provider value={contextValue}>
+        {children}
+      </OrderContext.Provider>
+    </div>
+  );
+};
+
+export default layout;
