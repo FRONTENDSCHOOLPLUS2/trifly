ec3f35be225c2ea7a4bdd56e14a58b657fbcaa9c
전희선 <designh2sun@gmail.com>
2024-08-15 22:33:33 +0900

    ✨ feat: 탑승자 티켓 출력 화면 띄우기

---
+++ src/app/(primary)/reservation/[reservationDetail]/TicketLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Badge from "@/components/Badge/Badge";
+import { useEffect } from "react";
+
+const TicketLink = ({ id }: { id: string }) => {
+  const popupW = 412;
+  const popupH = 972;
+  const left = Math.ceil((window.screen.width - popupW) / 2);
+  const top = Math.ceil((window.screen.height - popupH) / 2);
+
+  const handleClick = () => {
+    window.open(
+      `/e-ticket/${id}`,
+      "_blank",
+      "width=" +
+        popupW +
+        ",height=" +
+        popupH +
+        ",left=" +
+        left +
+        ",top=" +
+        top +
+        ""
+    );
+  };
+  return (
+    <div onClick={() => handleClick()}>
+      <Badge>티켓 확인하기</Badge>
+    </div>
+  );
+};
+
+export default TicketLink;
