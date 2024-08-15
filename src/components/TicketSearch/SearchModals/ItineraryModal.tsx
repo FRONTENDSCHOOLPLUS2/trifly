b14b2f0d92948186175e31c64ea094790ee9bfc5
JWJung-99 <39busy@naver.com>
2024-08-15 15:24:21 +0900

    ✨ feat: 날짜 선택 기능 생성

--- src/components/TicketSearch/SearchModals/ItineraryModal.tsx
+++ src/components/TicketSearch/SearchModals/ItineraryModal.tsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import "./ItineraryModal.scss";
+import Calendar from "react-calendar";
 
 interface ItineraryModalProps {
   handleClose: (a: boolean) => void;
 }
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 const ItineraryModal = ({ handleClose }: ItineraryModalProps) => {
+  const [date, setDate] = useState<Value>(new Date());
+
+  console.log(date);
+
   return (
     <div className="itinerary-modal search-modal-contents">
       <button type="button" onClick={() => handleClose(false)}>
         <img src="/img/icon-close-black.svg" alt="닫기" />
         <span className="hidden">닫기</span>
       </button>
+
+      <div className="calendar">
+        <Calendar
+          onChange={setDate}
+          value={date}
+          locale="ko-KR"
+          calendarType="gregory"
+          prev2Label={null}
+          next2Label={null}
+          minDate={new Date()}
+          showDoubleView={true}
+          // selectRange={true}
+        />
+      </div>
     </div>
   );
 };
