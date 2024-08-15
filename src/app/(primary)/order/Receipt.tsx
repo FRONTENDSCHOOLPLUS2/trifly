0b4847a23c025f08fca3588384f176a69afba1c5
s0zzang <dlth8888@gmail.com>
2024-08-15 17:51:20 +0900

    ✅ done: 결제 - 영수증

--- src/app/(primary)/order/Receipt.tsx
+++ src/app/(primary)/order/Receipt.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { orderState } from "@/atoms/atoms";
+import { useState } from "react";
 import { useRecoilValue } from "recoil";
 
 type PersonalPrice = {
@@ -13,74 +14,108 @@ type PersonalPrice = {
   refundableTaxes: string;
 }[];
 
+const getLocalNum = (value: string | number) => {
+  if (typeof value === "string") value = +value;
+  return value.toLocaleString();
+};
+
 const Receipt = () => {
   const { price, totalPrice } = useRecoilValue(orderState);
+  const charge = 10000;
   const personalPrice: PersonalPrice[] = [[], [], []];
   const personalIdx = {
     ADULT: 0,
     CHILD: 1,
     INFANT: 2,
   };
-  price.map((item) => {
-    console.log(personalPrice[personalIdx[item.travelerType]]);
-    personalPrice[personalIdx[item.travelerType]].push(item.price);
-  });
+  price.map((item) =>
+    personalPrice[personalIdx[item.travelerType]].push(item.price)
+  );
+  const [isDetailShow, setIsDetailShow] = useState(false);
 
   return (
     <article className="receipt">
       <h3>요금 영수증</h3>
       <dl>
         <dt>결제 예정 금액</dt>
-        <dd>{totalPrice}원</dd>
-        <dd className="sub">유류할증료 및 기타 요금 포함</dd>
+        <dd>
+          {getLocalNum(totalPrice)}원
+          <span className="sub">유류할증료 및 제세공과금 포함</span>
+        </dd>
       </dl>
 
       <div className="price-detail-box">
-        <button type="button">요금 정보 상세보기</button>
+        <button
+          type="button"
+          className={`show-btn ${isDetailShow ? "act" : ""}`}
+          onClick={() => setIsDetailShow(!isDetailShow)}
+        >
+          {isDetailShow ? "요금 정보 닫기" : `요금 정보 상세보기`}
+        </button>
 
-        {personalPrice.map((item, idx) => {
-          const type = idx === 0 ? "성인" : idx === 1 ? "소아" : "유아";
-          const count = item.length;
-          if (!count) return;
+        {isDetailShow && (
+          <div className="price-detail">
+            {personalPrice.map((item, idx) => {
+              const count = item.length;
+              if (!count) return;
+              const type = idx === 0 ? "성인" : idx === 1 ? "소아" : "유아";
+              let YR = 0;
+              let taxes = 0;
 
-          return (
-            <>
-              <h4>
-                <span>
-                  {type} <strong>{count}</strong>명
-                </span>
-                <span>{+item[0].total * count}원</span>
-              </h4>
-              <table className="price-adult">
-                <caption>{type} 요금 정보</caption>
-                <tbody>
-                  <tr>
-                    <td>항공요금</td>
-                    <td>{count}명</td>
-                    <td>5,520원</td>
-                  </tr>
-                  <tr>
-                    <td>유류할증료</td>
-                    <td>{count}명</td>
-                    <td>11,000원</td>
-                  </tr>
-                  <tr>
-                    <td>제세공과금</td>
-                    <td>{count}명</td>
-                    <td>4,000원</td>
-                  </tr>
-                  <tr>
-                    <td>발권 수수료</td>
-                    <td>{count}명</td>
-                    <td>
-                      <s></s>1,000원
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </>
-          );
-        })}
+              item[0].taxes.map((tax) => {
+                if (tax.code === "YR") {
+                  YR = +tax.amount;
+                } else {
+                  taxes += +tax.amount;
+                }
+              });
+
+              return (
+                <div key={idx} className="price">
+                  <h4>
+                    <span>
+                      {type} <strong>{count}</strong>명
+                    </span>
+                    <span>
+                      {getLocalNum(+item[0].total * count + charge * count)}원
+                    </span>
+                  </h4>
+                  <table className="price-adult">
+                    <caption>{type} 요금 정보</caption>
+                    <tbody>
+                      <tr>
+                        <td className="tit">항공요금</td>
+                        <td className="count">{count}명</td>
+                        <td className="price">
+                          {getLocalNum(+item[0].base * count)}원
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="tit">유류할증료</td>
+                        <td className="count">{count}명</td>
+                        <td className="price">{getLocalNum(YR * count)}원</td>
+                      </tr>
+                      <tr>
+                        <td className="tit">제세공과금</td>
+                        <td className="count">{count}명</td>
+                        <td className="price">
+                          {getLocalNum(taxes * count)}원
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="tit">발권 수수료</td>
+                        <td className="count">{count}명</td>
+                        <td className="price">
+                          {getLocalNum(charge * count)}원
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </article>
   );
