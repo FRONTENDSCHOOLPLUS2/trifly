85b4bc9ca8e647adc73b9b6e9c5f7982abbc76e2
JWJung-99 <39busy@naver.com>
2024-08-17 15:47:43 +0900

    🔒 type: env 타입 설정

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,9 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_MARKET_API_SERVER: string;
     NEXT_PUBLIC_MARKET_API_CLIENT_ID: string;
+    NEXT_PUBLIC_AMADEUS_API_SERVER: string;
+    NEXT_PUBLIC_AMADEUS_CLIENT_ID: string;
+    NEXT_PUBLIC_AMADEUS_CLIENT_SECRET: string;
     NEXT_PUBLIC_LIMIT: string;
     KAKAO_CLIENT_ID: string;
     KAKAO_CLIENT_SECRET: string;
