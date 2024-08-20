d70d8b703be72d90b83d14ad4b92f6baee3945ad
s0zzang <dlth8888@gmail.com>
2024-08-20 14:23:33 +0900

    🐛 fix: env 타입 정의

--- src/auth.ts
+++ src/auth.ts
@@ -53,7 +53,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
 
-  secret: process.env.NEXT_PUBLIC_AUTHSECRET,
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
 
   session: {},
 
