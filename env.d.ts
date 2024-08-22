c5e76224216071bdd3d49e55a99014a626ea0b60
JWJung-99 <39busy@naver.com>
2024-08-22 20:33:35 +0900

    🔧 conf: env.d.ts 파일 수정

--- env.d.ts
+++ env.d.ts
@@ -10,7 +10,6 @@ declare namespace NodeJS {
     KAKAO_CLIENT_SECRET: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
-    NEXT_PUBLIC_AUTH_SECRET: string;
-    NEXT_PUBLIC_CHARGE: number;
+    AUTH_SECRET: string;
   }
 }
