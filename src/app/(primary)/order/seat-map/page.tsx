1e9a8b26086736a4832f3f5bd33b7f8285279c45
전희선 <designh2sun@gmail.com>
2024-08-22 11:40:38 +0900

    🚧 cont: 비상구 class 적용 삼항식 <span> -> null

--- src/app/(primary)/order/seat-map/page.tsx
+++ src/app/(primary)/order/seat-map/page.tsx
@@ -78,9 +78,7 @@ const Page = () => {
                       >
                         {cell.number}
                       </button>
-                    ) : (
-                      <span />
-                    )}
+                    ) : null}
                   </td>
                 ))}
               </tr>
