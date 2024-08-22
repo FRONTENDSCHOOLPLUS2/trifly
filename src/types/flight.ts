8f8b00e3cb802a8328969d2b78c45e2b453cdb57
JWJung-99 <39busy@naver.com>
2024-08-22 20:22:04 +0900

    🔒 type: flight 타입 수정

--- src/types/flight.ts
+++ src/types/flight.ts
@@ -56,6 +56,8 @@ export interface OffersSearchData extends OffersData {
           quantity?: number;
         };
         includedCabinBags?: {
+          weight?: number;
+          weightUnit?: "KG";
           quantity?: number;
         };
         amenities?: {
