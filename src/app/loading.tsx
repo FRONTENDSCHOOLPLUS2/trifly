494b508cbbf7f8ee56799828ec6361864740a2da
전희선 <designh2sun@gmail.com>
2024-08-27 02:23:51 +0900

    🐛 fix: Loading 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,29 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import LoadingSpinner from "@/components/Loading/LoadingSpinner";
+import Lottie from "react-lottie-player";
+import lottieJson from "../../public/lottie/loading.json";
 import React from "react";
 
 const Loading = () => {
-  const Lottie = dynamic(() => import("react-lottie-player"), {
-    ssr: false,
-  });
-  return <Lottie loop play animationData={LoadingSpinner} />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      <Lottie
+        loop
+        play
+        animationData={lottieJson}
+        style={{ width: 150, height: 150 }}
+      />
+    </div>
+  );
 };
 
 export default Loading;
