cbb74eac675ce7834c1f2c969fe316864bd18991
JWJung-99 <39busy@naver.com>
2024-08-26 15:34:24 +0900

    🚧 cont: 항공권 로딩 화면 개발 중

--- src/app/(default)/(ticket)/ticket-result/Loading.tsx
+++ src/app/(default)/(ticket)/ticket-result/Loading.tsx
@@ -1,8 +1,24 @@
-import "./Loading.scss";
+"use client";
+
+import { useRecoilValue } from "recoil";
+import "./TicketLoading.scss";
+import { searchResultState } from "@/atoms/atoms";
 
 // 항공권 검색 로딩 화면
-const Loading = () => {
-  return <section className="ticket-searching">로딩 중</section>;
+const TicketLoading = () => {
+  const searchResult = useRecoilValue(searchResultState);
+  const destination = searchResult.destination;
+
+  console.log(destination);
+
+  return (
+    <section className={`ticket-searching`}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo fugit
+      incidunt hic, voluptas dignissimos voluptatem! Voluptatum at minima
+      perferendis corrupti accusantium soluta illo vitae dolorum numquam?
+      Sapiente laudantium vitae commodi!
+    </section>
+  );
 };
 
-export default Loading;
+export default TicketLoading;
