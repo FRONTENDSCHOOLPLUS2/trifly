e1cd34b62e67d40d67ab01a7db53305debb0f515
JWJung-99 <39busy@naver.com>
2024-08-26 19:45:05 +0900

    🚧 cont: 항공권 로딩 컴포넌트 작업 중

--- src/app/(default)/(ticket)/ticket-result/Loading.tsx
+++ src/app/(default)/(ticket)/ticket-result/Loading.tsx
@@ -3,15 +3,17 @@
 import { useRecoilValue } from "recoil";
 import "./TicketLoading.scss";
 import { searchResultState } from "@/atoms/atoms";
+import { useSearchParams } from "next/navigation";
 
 // 항공권 검색 로딩 화면
 const TicketLoading = () => {
-  const searchResult = useRecoilValue(searchResultState);
-  const { origin, destination } = searchResult;
+  const searchParams = useSearchParams();
+  const origination = searchParams.get("originLocationCode");
+  const destination = searchParams.get("destinationLocationCode");
 
   return (
     <section className="ticket-searching">
-      {origin.code} - {destination.code}
+      {origination} - {destination}
     </section>
   );
 };
