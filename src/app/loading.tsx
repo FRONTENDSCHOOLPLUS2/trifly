c60a291abd541655fe1589c0be81780558a3ce9c
전희선 <designh2sun@gmail.com>
2024-08-26 11:53:31 +0900

    ✅ done: next loading.tsx 파일 추가, dynamic으로 ssr:false로 지정

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import LoadingSpinner from "@/components/Loading/LoadingSpinner";
+import React from "react";
+
+const Loading = () => {
+  const Lottie = dynamic(() => import("react-lottie-player"), {
+    ssr: false,
+  });
+  return <Lottie loop play animationData={LoadingSpinner} />;
+};
+
+export default Loading;
