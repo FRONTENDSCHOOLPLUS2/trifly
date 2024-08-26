c3b67f61d10e945c7521dd156cbdd90992b87b9c
s0zzang <dlth8888@gmail.com>
2024-08-26 23:43:55 +0900

    ✅ done: 티켓 검색 로딩 화면

--- src/app/(default)/(ticket)/ticket-result/Loading.tsx
+++ src/app/(default)/(ticket)/ticket-result/Loading.tsx
@@ -1,19 +1,54 @@
 "use client";
 
-import { useRecoilValue } from "recoil";
-import "./TicketLoading.scss";
-import { searchResultState } from "@/atoms/atoms";
+import { AirportData, CodeState } from "@/types";
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
+import "./TicketLoading.scss";
 
 // 항공권 검색 로딩 화면
-const TicketLoading = () => {
+const TicketLoading = ({ code }: { code: CodeState<AirportData> }) => {
+  const URL = process.env.NEXT_PUBLIC_MARKET_API_SERVER;
+
   const searchParams = useSearchParams();
-  const origination = searchParams.get("originLocationCode");
-  const destination = searchParams.get("destinationLocationCode");
+  const originLocationCode = searchParams.get("originLocationCode")!;
+  const destinationLocationCode = searchParams.get("destinationLocationCode")!;
+  const departureDate = searchParams.get("departureDate")!;
+  const returnDate = searchParams.get("returnDate")!;
 
   return (
     <section className="ticket-searching">
-      {origination} - {destination}
+      <div className="img-box">
+        <Image
+          src={`${URL}${code[destinationLocationCode].img}`}
+          alt={code[destinationLocationCode].value}
+          width={0}
+          height={0}
+          sizes="100%"
+        />
+      </div>
+      <div className="title-box">
+        <h3>
+          <strong className="bold">{code[originLocationCode].value}</strong>
+          에서부터{" "}
+          <strong className="bold">
+            {code[destinationLocationCode].value}
+          </strong>
+          까지의 <br />
+          왕복 여정을 찾는중입니다
+        </h3>
+      </div>
+      <ul className="itinerary-box">
+        <li className="origin">
+          <span className="eng bold">{originLocationCode}</span>
+          <span className="kor">{code[originLocationCode].value}</span>
+          <span className="date">{departureDate}</span>
+        </li>
+        <li className="destination">
+          <span className="eng bold">{destinationLocationCode}</span>
+          <span className="kor">{code[destinationLocationCode].value}</span>
+          <span className="date">{returnDate}</span>
+        </li>
+      </ul>
     </section>
   );
 };
--- src/app/(default)/(ticket)/ticket-result/layout.tsx
+++ src/app/(default)/(ticket)/ticket-result/layout.tsx
@@ -1,14 +1,17 @@
 import { Suspense } from "react";
 import TicketLoading from "./Loading";
+import { fetchCodes } from "@/data/fetch/fetchCode";
+import { AirportData, CodeState } from "@/types";
 
-export default function TicketLayout({
+export default async function TicketLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { code } = await fetchCodes<AirportData>();
   return (
     <div>
-      <Suspense fallback={<TicketLoading />}>{children}</Suspense>
+      <Suspense fallback={<TicketLoading code={code} />}>{children}</Suspense>
     </div>
   );
 }
