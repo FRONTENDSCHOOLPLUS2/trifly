ee5be6b79a7828f8821d0a611fb843864c361d7b
전희선 <designh2sun@gmail.com>
2024-08-27 15:06:27 +0900

    ✅ done: SeatmapGroup으로 code fetch 후 prop

--- src/app/(primary)/order/seat-map/[id]/page.tsx
+++ src/app/(primary)/order/seat-map/[id]/page.tsx
@@ -2,13 +2,16 @@ import "./seatmapSelect.scss";
 import { FetchOrder } from "@/lib/fetchOrder";
 
 import SeatmapGroup from "./SeatmapGroup";
+import { fetchCodes } from "@/data/fetch/fetchCode";
+import { AircraftData } from "@/types";
 
 const Page = async ({ params: { id } }: { params: { id: string } }) => {
   const dataList = await FetchOrder();
   const data = dataList.item.find((item) => item.reservationId === id);
   const orderId = data!._id;
+  const { code } = await fetchCodes<AircraftData>();
 
-  return <SeatmapGroup data={data} id={id} orderId={orderId} />;
+  return <SeatmapGroup data={data} id={id} orderId={orderId} code={code} />;
 };
 
 export default Page;
