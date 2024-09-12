b995fd43843e9421c5d8f8547005cf049aed774e
JWJung-99 <39busy@naver.com>
2024-09-12 16:04:37 +0900

    🚧 cont: 필터 설정 값 저장 기능 진행 중

--- src/RecoilWrapper.tsx
+++ src/RecoilWrapper.tsx
@@ -2,11 +2,13 @@
 
 import { RecoilRoot } from "recoil";
 import Modal from "@/components/Modal/Modal";
+import FilterStateManager from "./FilterStateManager";
 
 const RecoilRootWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <RecoilRoot>
       {children}
+      <FilterStateManager />
       <Modal />
     </RecoilRoot>
   );
