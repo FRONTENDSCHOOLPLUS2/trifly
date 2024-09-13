04a61e6bc1bc3f73d31e02184e91317b08a070cd
전희선 <designh2sun@gmail.com>
2024-09-13 21:58:23 +0900

    ♻️ refactor: pagination number에 따라 변경된 params값으로 data 불러오기 완료

--- src/app/(primary)/reservation/page.tsx
+++ src/app/(primary)/reservation/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import ReservationList from "./ReservationList";
-import Pagination from "./Pagination";
 import Button from "@/components/Button/Button";
+import { FetchOrderList } from "@/lib/fetchOrder";
+import OrdersItem from "./OrdersItem";
+import "./reservation.scss";
+import Pagination from "./Pagination";
 
-const page = () => {
+const PageContainer = async ({
+  searchParams: { page },
+}: {
+  searchParams: { page: string };
+}) => {
+  const data = await FetchOrderList(page);
+
+  const reservationData = data.item;
+
   return (
     <div>
       <div className="reservation-header">
@@ -15,10 +25,31 @@ const page = () => {
           <Button>검색</Button>
         </div>
       </div>
-      <ReservationList />
-      <Pagination />
+
+      <section>
+        <table className="reservation-table">
+          <caption className="hidden">예약 내역 리스트</caption>
+          <thead>
+            <tr>
+              <th>예약 번호</th>
+              <th>예약일</th>
+              <th>출발</th>
+              <th>도착</th>
+              <th>왕복/편도</th>
+              <th>출발일</th>
+              <th>인원</th>
+              <th>총 금액</th>
+            </tr>
+          </thead>
+
+          {reservationData?.map((item) => (
+            <OrdersItem key={item._id} item={item} />
+          ))}
+        </table>
+      </section>
+      <Pagination {...data.pagination!} />
     </div>
   );
 };
 
-export default page;
+export default PageContainer;
