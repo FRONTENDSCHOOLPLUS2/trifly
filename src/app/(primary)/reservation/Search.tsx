3593e67801f6326eaa8e0d344b272276e774a83b
전희선 <designh2sun@gmail.com>
2024-09-19 19:11:06 +0900

    ✅ done: 검색창 기능 구현 완료

--- src/app/(primary)/reservation/Search.tsx
+++ src/app/(primary)/reservation/Search.tsx
@@ -6,7 +6,7 @@ function SearchKeyWords() {
       <input
         type="text"
         placeholder="예약번호를 입력해주세요"
-        className={"search-input"}
+        className="search-input"
         name="keyword"
       />
       <button type="submit" className="search-button">
