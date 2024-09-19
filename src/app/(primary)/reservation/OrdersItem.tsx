27c1edadff7bd531292dcb3599bd9f88800f8fee
전희선 <designh2sun@gmail.com>
2024-09-19 19:10:21 +0900

    ♻️ refactor: 불필요한 코드 삭제

--- src/app/(primary)/reservation/OrdersItem.tsx
+++ src/app/(primary)/reservation/OrdersItem.tsx
@@ -3,7 +3,6 @@
 import Badge from "@/components/Badge/Badge";
 import { OrderItem } from "@/types";
 import { useRouter } from "next/navigation";
-import Pagination from "./Pagination";
 
 const OrdersItem = ({ item }: { item: OrderItem }) => {
   const router = useRouter();
@@ -13,40 +12,38 @@ const OrdersItem = ({ item }: { item: OrderItem }) => {
   };
 
   return (
-    <>
-      <tbody>
-        <tr onClick={() => handleClick(item._id)} style={{ cursor: "pointer" }}>
-          <td className="reservation-number">
-            <h3>{item.reservationId.substring(0, 6)}</h3>
-          </td>
-          <td className="reservation-date">
-            {item.createdAt.substring(0, 10).replaceAll(".", "-")}
-          </td>
-          <td className="departure">
-            {item.itineraries[0].segments[0].departure.iataCode}
-          </td>
-          <td className="arrival">
-            {item.itineraries[0].segments.length === 2
-              ? item.itineraries[0].segments[1].arrival.iataCode
-              : item.itineraries[0].segments[0].arrival.iataCode}
-          </td>
-          <td className="travel">
-            {item.itineraries.length === 2 ? (
-              <Badge>왕복</Badge>
-            ) : (
-              <Badge type="secondary">편도</Badge>
-            )}
-          </td>
-          <td className="schedule">
-            {item.itineraries[0].segments[0].departure.at.substring(0, 10)}
-          </td>
-          <td className="personnel">{item.passengers?.length}</td>
-          <td className="total">
-            {Number(item.totalPrice).toLocaleString("ko-KR")} 원
-          </td>
-        </tr>
-      </tbody>
-    </>
+    <tbody>
+      <tr onClick={() => handleClick(item._id)} style={{ cursor: "pointer" }}>
+        <td className="reservation-number">
+          <h3>{item.reservationId.substring(0, 6)}</h3>
+        </td>
+        <td className="reservation-date">
+          {item.createdAt.substring(0, 10).replaceAll(".", "-")}
+        </td>
+        <td className="departure">
+          {item.itineraries[0].segments[0].departure.iataCode}
+        </td>
+        <td className="arrival">
+          {item.itineraries[0].segments.length === 2
+            ? item.itineraries[0].segments[1].arrival.iataCode
+            : item.itineraries[0].segments[0].arrival.iataCode}
+        </td>
+        <td className="travel">
+          {item.itineraries.length === 2 ? (
+            <Badge>왕복</Badge>
+          ) : (
+            <Badge type="secondary">편도</Badge>
+          )}
+        </td>
+        <td className="schedule">
+          {item.itineraries[0].segments[0].departure.at.substring(0, 10)}
+        </td>
+        <td className="personnel">{item.passengers?.length}</td>
+        <td className="total">
+          {Number(item.totalPrice).toLocaleString("ko-KR")} 원
+        </td>
+      </tr>
+    </tbody>
   );
 };
 
